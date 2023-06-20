1c1b79984a00a05ead137f7f7515114624b8131f
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-06-19 22:17:57 -0300

    update command

--- src/services/cria-usuario-update-command.service.js
+++ src/services/cria-usuario-update-command.service.js
@@ -6,7 +6,8 @@ exports.criaComandoUpdate = function (email, mercadoPagoId) {
             }
         },
         ExpressionAttributeValues: {
-            ":mercado_pago_id": { S: mercadoPagoId }
+            ":mercado_pago_id": { S: mercadoPagoId },
+            ":email": { S: email }
         },
         UpdateExpression: "SET mercado_pago_id = :mercado_pago_id",
         ConditionExpression: "attribute_exists(email) AND email = :email"
